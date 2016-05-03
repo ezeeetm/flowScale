cbd2524bc5efb30364ace458959cf50153cf9e63
Eric Miller <emiller@nexosis.com>
2016-05-03 19:45:41 -0400

    right in the middle of agressive refactoring

--- flowScale.py
+++ flowScale.py
@@ -2,14 +2,76 @@
 import json
 import boto3
 import time
+import collections
+import sys
 
-def GetConfig():
+
+def Debug ( headerMsg, debugMsgDict ):
+    if debug:
+        if headerMsg:
+            print("############## DEBUG: %s") % ( headerMsg )
+        for i in debugMsgDict:
+            print "%s: %s" % (i, debugMsgDict[i])
+
+# munges up values from params and config.json to produce a meta config object can be passed around idempotently
+def ConfigFactory( event ):
     with open('config.json', 'r') as configFile:
         configStr = configFile.read()
         configJson = json.loads(configStr)
-        return configJson
 
-def InitStateTable ( client, dynamodb, stateTableName, stateTableInitialReadCapacityUnits, stateTableInitialWriteCapacityUnits, config ):
+        global debug # yo dawg...
+        debug = configJson['globalConfig']['debug']
+        Debug ( sys._getframe().f_code.co_name, {'params':event} )
+        region = configJson['globalConfig']['region']
+        stateTableInitialReadCapacityUnits = configJson['globalConfig']['stateTableInitialReadCapacityUnits']
+        stateTableInitialWriteCapacityUnits = configJson['globalConfig']['stateTableInitialWriteCapacityUnits']
+        stateTableName = configJson['globalConfig']['stateTable']
+        action = event['action']
+
+        if action == 'healthcheck':
+            stack, uid, stackId = None, None, None
+        else:
+            stack = event['stack']
+            uid = event['uid']
+            stackId = "%s-%s" % (stack, uid)
+
+        if action == 'update':
+            stateId = event['stateId']
+            desiredState = configJson['stacks'][stackId][stateId]
+        else:
+            stateId, desiredState = None, None
+
+        configObj = collections.namedtuple('Config', [
+            'debug',
+            'region',
+            'stateTableName',
+            'stateTableInitialReadCapacityUnits',
+            'stateTableInitialWriteCapacityUnits',
+            'stackId',
+            'action',
+            'stateId',
+            'desiredState'
+        ])
+
+        config = configObj(
+            debug,
+            region,
+            stateTableName,
+            stateTableInitialReadCapacityUnits,
+            stateTableInitialWriteCapacityUnits,
+            stackId,
+            action,
+            stateId,
+            desiredState
+        )
+
+        Debug ( None, config._asdict() )
+        return config
+
+
+#def InitStateTable ( client, dynamodb, conf ):
+
+'''
     try:
         resp = client.describe_table(TableName=stateTableName)
         print("stateTable %s status: %s") % ( stateTableName, resp['Table']['TableStatus'])
@@ -57,75 +119,11 @@ def InitStateTable ( client, dynamodb, stateTableName, stateTableInitialReadCapa
 
     stateTable = dynamodb.Table(stateTableName)
     return stateTable
-
-def HeartBeat ( stateTable, stackId ):
-    now = int(time.time())
-    print("HeartBeat for stack %s @ %s epoch time") % ( stackId, now )
-    response = stateTable.update_item(
-        Key={
-            'stackId': stackId
-        },
-        UpdateExpression="set lastHeartBeatTime = :lh",
-        ExpressionAttributeValues={
-            ':lh': now
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-    print("UpdateItem succeeded: %s") % ( response )
-    return
-
-def HealthCheck ( stackId ):
-    print("HealthCheck: %s") % ( stackId )
-    return
+'''
 
 
 def lambda_handler ( event, context ):
-    # infer config values from params and config.json
-    config = GetConfig()
-    debug = config['globalConfig']['debug']
-    region = config['globalConfig']['region']
-    stateTableName = config['globalConfig']['stateTable']
-    stateTableInitialReadCapacityUnits = config['globalConfig']['stateTableInitialReadCapacityUnits']
-    stateTableInitialWriteCapacityUnits = config['globalConfig']['stateTableInitialWriteCapacityUnits']
-    stack = event['stack']
-    uid = event['uid']
-    eventType = event['type']
-    stackId = "%s-%s" % (stack, uid)
-
-    # dynamo connection object, do this at app start only, not for each operation. Creates admin table to manage state, if it does not exist
-    client = boto3.client('dynamodb', region_name=region)
-    dynamodb = boto3.resource('dynamodb', region_name=region)
-    stateTable = InitStateTable ( client, dynamodb, stateTableName, stateTableInitialReadCapacityUnits, stateTableInitialWriteCapacityUnits, config )
-
-    if eventType == 'heartbeat':
-        HeartBeat ( stateTable, stackId )
-        return
-    elif eventType == 'healthcheck':
-        HealthCheck ( stackId )
-        return
-    elif eventType == 'update':
-        scenarioId = event['scenario']
-        scenario = config['stackConfigs'][stackId][scenarioId]
-    else:
-        print('This should never happen, how did you get here?')
-
-    if debug == True:
-        print("stackId: %s") % ( stackId )
-        print("scenarioId: %s") % ( scenarioId )
-        print("scenario: %s") % ( scenario )
-        for changeGroup in scenario:
-            print("changeGroup: %s") % ( changeGroup )
-
-'''
-#todo
-if action = heartbeat update last hearbeat time
-heartbeat threshold to config
-add initial heartbeat to InitStateTable
-scheduled call with type heartbeat check if over configured threshold, change scenario to 'idle'
-
-ensure request to change scenarios is ignored when already in that scenario
-
-cw log function
-test to ensure each stack has an idle scenario
-function to ensure all tables have a workflow idle rule, with exclude list, SNS alert when this is not the case
-'''
+    conf = ConfigFactory ( event )
+    client = boto3.client('dynamodb', region_name=conf.region) # ddb client object, used for high level operations: describe/create tables, etc.
+    dynamodb = boto3.resource('dynamodb', region_name=conf.region) # ddb table object, used for table read/write/update/delete operations
+    #InitStateTable ( client, dynamodb, conf ) #stateTable =
